b20bb3dae0818e74cd9876a61d37aac37ceb2c49
chansida14 <47920637+chansida14@users.noreply.github.com>
2021-09-07 19:01:00 +0700

    Add files via upload

--- src/main/java/Rational.java
+++ src/main/java/Rational.java
@@ -1,100 +1,104 @@
-class Rational { 
-    long numerator,denominator; 
-
-    class Illegal extends Exception { 
-        String reason; 
-        Illegal (String reason) { 
-            this.reason = reason; 
-        } 
-    } 
-
+class Rational {
+    long numerator , denominator ;
+    class Illegal extends Exception {
+        String reason;
+        Illegal (String reason) {
+            this.reason = reason;
+        }
+    }
     Rational() {
         // to be completed
+        denominator = 1;
+        numerator = 1;
+
+        if (denominator==0){
+            throw new RuntimeException("The value of denominator is zero");
+        }
+    }
+    Rational(long numerator , long denominator ) throws Illegal {
+
+        denominator = this.denominator;
+
+        numerator = this.numerator;
+
+        if (denominator==0){
+            throw new RuntimeException("The value of denominator is zero");
+        }
+
+    }
+    private void simplestForm () {
+        long computeGCD ;
+        computeGCD = GCD(Math.abs( numerator ), denominator );
+        numerator /= computeGCD ;
+        denominator /= computeGCD ;
+    }
+    // find the greatest common denominator
+    private long GCD(long a, long b) {
+        if (a%b ==0) return b;
+        else return GCD(b,a%b);
     }
 
-    Rational(long numerator, long denominator) throws Illegal { 
-        // to be completed
-    } 
+public void add(Rational x) {
+    numerator = ( numerator * x. denominator ) + (x. numerator * denominator );
+    denominator = ( denominator * x. denominator );
+    simplestForm ();
+}
 
-    // find the reduce form 
-    private void simplestForm() { 
-        long computeGCD; 
-        computeGCD = GCD(Math.abs(numerator), denominator); 
-        numerator /= computeGCD; 
-        denominator /= computeGCD; 
-    } 
-
-    // find the greatest common denominator 
-    private long GCD(long a, long b) { 
-        if (a%b ==0) return b; 
-        else return GCD(b,a%b); 
-    }
-
-    /***
-     * Compute an addition of the current rational number to another given rational number
-     * @param x the rational number to be added to the current rational number
-     */
-    public void add(Rational x) { 
-        numerator = (numerator * x.denominator) + (x.numerator * denominator);
-        denominator = (denominator * x.denominator); 
-        simplestForm(); 
-    }
-
-    /***
-     * Compute a subtraction of the current rational number to another given rational number
-     * @param x the rational number to be subtracted from the current rational number
-     */
     public void subtract(Rational x) {
-        // to be completed
+        numerator = ( numerator * x. denominator ) - (x. numerator * denominator );
+        denominator = ( denominator * x. denominator );
+        simplestForm ();    }
+
+    public void multiply(Rational x) {
+
+        denominator = denominator * x.denominator;
+
+        numerator = numerator * x.numerator;
+
+        simplestForm();
     }
 
-    /***
-     * Compute a multiplication of the current rational number to another given rational number
-     * @param x the rational number to be multiplied to the current rational number
-     */
-    public void multiply(Rational x) { 
-        // to be completed
-    }
-
-    /***
-     * Compute a division of the current rational number to another given rational number
-     * @param x the rational number to be divided by the current rational number
-     */
     public void divide(Rational x) {
-        // to be completed
+        denominator = denominator * x.numerator;
+
+        numerator = numerator * x.denominator;
+
+        simplestForm();
     }
 
-    /***
-     * Check if the given rational number equals to the current rational number
-     * @param x the rational number to be compared to the current rational number
-     * @return true if the given rational number equals to the current, false otherwise
-     */
     public boolean equals(Object x) {
-        // to be completed
-        return true; // TODO: This needs to be modified.
+
+        Rational a = Rational.class.cast(x);
+        simplestForm();
+        a.simplestForm();
+        if(a.numerator == numerator&& a.denominator == denominator){
+            return true;
+        }
+        else
+            return false;
+
     }
 
-    /***
-     * Compare the current rational number to the current rational number
-     * @param x the rational number to be compared to the current rational number
-     * @return -1 if the current rational number is less than the given number, 0 if they're equal, 1 if the current
-     * rational number is larger than the given number
-     */
     public long compareTo(Object x) {
         // to be completed
-        return -1; // TODO: this needs to be modified.
+        Rational a = Rational.class.cast(x);
+        simplestForm();
+        a.simplestForm();
+        //less
+        if ((numerator*a.denominator)<(denominator*a.numerator)){
+            return -1;
+        } else if ((numerator*a.denominator)>(denominator*a.numerator)){
+            return 1;
+        } else
+            return 0;
     }
 
-    /***
-     * Give the formatted string of the rational number
-     * @return the string representation of the rational number. For example, "1/2", "3/4".
-     */
-    public String toString() { 
-        // to be completed
-        return ""; // TODO: This needs to be modified.
+    public String toString () {
+        return numerator + "/" + denominator;
     }
 
-    public static void main(String[] args) {
+
+    public static void main(String [] args) {
         System.out.println("This is Rational class.");
     }
 }
